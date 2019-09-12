57f40b24cf75417d35d51e4d52a863366fd7a4b1
Michelle Chis <s1230292@monmouth.edu>
2019-09-11 20:14:52 -0400

    Good to go

--- GoForIt/src/GoodTry.java
+++ GoForIt/src/GoodTry.java
@@ -4,7 +4,7 @@ public class GoodTry {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello Hello World World! ! !");
+		System.out.println("Hello World. This is the second version");
 		
 	}
 
