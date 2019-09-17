be1d5dd5c0756d62f83ea96d37d8fdc10c07a040
Michelle Chis <s1230292@monmouth.edu>
2019-09-16 20:56:51 -0400

    Variables have been created.

---
+++ HousePainting/src/PaintCost.java
@@ -0,0 +1,34 @@
+
+public class PaintCost {
+
+	public static void main(String[] args) {
+		/*
+		 * This program will calculate the cost to paint a house.
+		 */
+		double houseLengthFeet;
+		double houseLengthInches;
+		double houseWidthFeet;
+		double houseWidthInches;
+		double houseHeightFeet;
+		double houseHeightInches;
+		double windowLengthFeet;
+		double windowLengthInches;
+		double windowWidthFeet;
+		double windowWidthInches;
+		int windowNumber;
+		double doorLengthFeet;
+		double doorLengthInches;
+		double doorWidthFeet;
+		double doorWidthInches;
+		int doorNumber;
+		double sqFtCost;
+		double sqFtPeak;
+		double sqFtNormal;
+		double sqFtWindows;
+		double sqFtDoors;
+		
+		
+		
+	}
+
+}
